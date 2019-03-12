cb0a479889dedbe121ca709ac82933707ce3f044
Lotanna <sugabelly@gmail.com>
2019-03-12 17:58:09 -0400

    changed file locations

---
+++ Astronomy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Astronomy
+//
+//  Created by Lotanna Igwe-Odunze on 3/12/19.
+//  Copyright © 2019 Lotanna Igwe-Odunze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Astronomy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Astronomy
+//
+//  Created by Lotanna Igwe-Odunze on 3/12/19.
+//  Copyright © 2019 Lotanna Igwe-Odunze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
